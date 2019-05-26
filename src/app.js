6e7da92969099150b538d8c00b0f4bd3088bce48
zyc <627245048@qq.com>
2019-05-27 00:34:20 +0800

    完成parcel配置和button的样式

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import PButton from './button'
+
+new Vue({
+    el:"#app",
+    components:{
+        PButton
+    }
+})
---
+++ src/button.vue
@@ -0,0 +1,38 @@
+<template>
+    <button class="p-button">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name:"PenButton"
+}
+</script>
+
+<style lang="stylus" scoped>
+ .p-button{
+    font-size: 14px;
+    color: #333;
+    padding: 12px 20px;
+    background: #fff;
+    border:1px solid #dcdfe6;
+    color: #606266;
+    border-radius: 4px;
+    outline: none;
+    cursor: pointer;
+    &:focus,
+    &:hover{
+        color: #409eff;
+        border-color: #c6e2ff;
+        background: #ecf5ff;
+    }
+    &:active{
+        color: #3a8ee6;
+        border-color:#3a8ee6; 
+    }
+}
+
+</style>
+
+
