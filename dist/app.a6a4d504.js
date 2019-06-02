4bc29176e28edb8d56f84f06bf07068602f16c0c
zyc <627245048@qq.com>
2019-06-02 13:39:29 +0800

    保存下

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -14286,11 +14286,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14271" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7324" + '/');
->>>>>>> dev-popover
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6531" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
